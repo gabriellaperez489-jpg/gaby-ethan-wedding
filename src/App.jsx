5878986306d8912d7775acef16694477f2b95d6e
gabriellaperez489-jpg <gabriellaperez489@gmail.com>
2026-04-15 17:06:52 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center", fontFamily: "Arial" }}>
+      <h1>Gabriela & Ethan</h1>
+      <p>We’re so excited to celebrate with you 🤍</p>
+      <p>Our wedding website is coming soon.</p>
+    </div>
+  );
+}
